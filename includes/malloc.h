3bc67e1a29e0b3a5a46fcd45191ad200b9144c60
Antoine MINEAU <amineau@e1r4p22.42.fr>
2016-12-26 19:49:07 +0100

    created structs and initialized

--- includes/malloc.h
+++ includes/malloc.h
@@ -6,7 +6,7 @@
 /*   By: amineau <amineau@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/03/22 09:27:29 by amineau           #+#    #+#             */
-/*   Updated: 2016/12/26 14:37:50 by amineau          ###   ########.fr       */
+/*   Updated: 2016/12/26 19:27:23 by amineau          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,5 +17,46 @@
 
 # define TINY getpagesize()
 # define SMALL getpagesize() * 10
+
+
+typedef struct	s_malloc
+{
+	void			*start;
+	void			*end;
+	struct s_malloc	*next;
+}				t_malloc;
+
+typedef struct	s_zone_tiny
+{
+	void				*zone;
+	void				*position;
+	size_t				max;
+	t_malloc			*alloc;
+	struct s_zone_tiny	*next;
+}				t_zone_tiny;
+
+typedef struct	s_zone_small
+{
+	void				*zone;
+	void				*position;
+	size_t				max;
+	t_malloc			*alloc;
+	struct s_zone_small	*next;
+}				t_zone_small;
+
+typedef struct	s_zone_large
+{
+	void				*zone;
+	void				*start;
+	void				*end;
+	struct s_zone_large	*next;
+}				t_zone_large;
+
+typedef struct	s_zone
+{
+	t_zone_tiny		*tiny;
+	t_zone_small	*small;
+	t_zone_large	*large;
+}				t_zone;
 
 #endif
--- srcs/main.c
+++ srcs/main.c
@@ -6,21 +6,96 @@
 /*   By: amineau <amineau@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/12/26 11:54:02 by amineau           #+#    #+#             */
-/*   Updated: 2016/12/26 15:11:41 by amineau          ###   ########.fr       */
+/*   Updated: 2016/12/26 19:48:09 by amineau          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "malloc.h"
 
-int test(char c)
+t_zone const	*zones(void)
 {
-	static char	*str;
-	int i = 0;
-	ft_printf("********\nstr : %s\naddr_before : %#x\n********", str, str);
-	str = (char*)mmap(0, TINY, PROT_READ | PROT_WRITE, MAP_ANON | MAP_PRIVATE, -1, 0);
-	while (i < TINY - 1)
-		str[i++] = c;
-	ft_printf("********\nstr : %s\naddr_after : %#x\nc : %c\n********\n", str, str, c);
+	static t_zone	*zone;
+
+	if (!zone)
+	{
+		if (!(zone = (t_zone*)mmap(0, sizeof(t_zone), PROT_READ | PROT_WRITE, MAP_ANON | MAP_PRIVATE, -1, 0))
+		|| !(zone->tiny = (t_zone_tiny*)mmap(0, sizeof(t_zone_tiny), PROT_READ | PROT_WRITE, MAP_ANON | MAP_PRIVATE, -1, 0))
+		|| !(zone->small = (t_zone_small*)mmap(0, sizeof(t_zone_small), PROT_READ | PROT_WRITE, MAP_ANON | MAP_PRIVATE, -1, 0)))
+			return (NULL);
+		zone->tiny->max = TINY;
+		zone->small->max = SMALL;
+		if (!(zone->tiny->zone = (void*)mmap(0, sizeof(void), PROT_READ | PROT_WRITE, MAP_ANON | MAP_PRIVATE, -1, 0))
+		|| !(zone->small->zone = (void*)mmap(0, sizeof(void), PROT_READ | PROT_WRITE, MAP_ANON | MAP_PRIVATE, -1, 0)))
+			return (NULL);
+		zone->tiny->next = NULL;
+		zone->small->next = NULL;
+		zone->tiny->position = zone->tiny->zone;
+		zone->small->position = zone->small->zone;
+	}
+	return (zone);
+}
+
+size_t	lenght_zone(size_t size)
+{
+	if (size <= 0)
+		return (NULL);
+	else if (size * 100 <= TINY)
+		return (TINY);
+	else if (size * 100 <= SMALL)
+		return (SMALL);
+	return (size);
+}
+
+t_malloc	*alloc(void *ptr, size_t size)
+{
+	t_malloc	*alloc;
+
+	if (!(alloc = (t_malloc*)mmap(0, sizeof(t_malloc), PROT_READ | PROT_WRITE, MAP_ANON | MAP_PRIVATE, -1, 0))
+	 || !(alloc->start = (void*)mmap(0, sizeof(void), PROT_READ | PROT_WRITE, MAP_ANON | MAP_PRIVATE, -1, 0))
+	 || !(alloc->end = (void*)mmap(0, sizeof(end), PROT_READ | PROT_WRITE, MAP_ANON | MAP_PRIVATE, -1, 0)))
+		return (NULL);
+	alloc->start = ptr;
+	alloc->end = ptr + size;
+	return (alloc);
+}
+
+void	*test(size_t size)
+{
+	size_t	lenght;
+	t_zone	*zone;
+	t_zone	*tmp;
+	t_malloc	*tmp_alloc;
+
+	zone = zones();
+	tmp = zone;
+	if (!(lenght = lenght_zone(size)))
+		return (NULL);
+	// Branche ou size est compris entre 1 et n
+	if (lenght == TINY)
+	{
+		// Trouver le premier maillon ou la taille peut rentrer
+		while (tmp->tiny && tmp->tiny->max < size)
+			tmp->tiny->next = tmp->tiny;
+		// Si ce maillon existe, ajouter l'allocation a ce maillon
+		if (tmp->tiny)
+		{
+			tmp_alloc = tmp->tiny->alloc;
+			// Parcours des allocations du maillon TINY (dans alloc (t_malloc*))
+			while (tmp_alloc && tmp_alloc->next && tmp_alloc->next->end - tmp_alloc->start < size)
+				tmp_alloc = tmp_alloc->next;
+			if (tmp_alloc)
+			{
+				//
+			}
+		}
+		// S'il n'existe pas, en creer un, puis ajouter l'allocation
+		else
+		{
+
+		}
+	}
+
+
 	return (0);
 }
 
